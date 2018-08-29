a01bf2e931e7b2d3eeb8bc755db27740336fecf1
Antonio <antoniodaria@msn.com>
2018-08-29 18:08:42 +0100

    Removing .each from print method)
    git status
    clear

--- directory.rb
+++ directory.rb
@@ -1,13 +1,19 @@
 def input_students
-  puts "Please enter the name of the students"
+  puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
+  # create an empty array
   students = []
+  # get the first name
   name = gets.chomp
+  # while the name is not empty, repeat this code
   while !name.empty? do
-    students << {:name => name, :cohort => :november}
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
+    # get another name from the user
     name = gets.chomp
   end
+  # return the array of students
   students
 end
 
@@ -17,22 +23,21 @@ def print_header
 end
 
 def print(students)
- students.select do |student|
-     if student[:name][0] == "a"
-          puts "#{student[:name]} (#{student[:cohort]} cohort)" 
-      end
+  count = 0
+  while count < 1 do
+   for i in students do
+      puts "#{i[:name]} #{i[:cohort]} cohort"
+   end
+  count+=1
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students, but only displaying those whose names begins with a "
+  puts "Overall, we have #{students.count} great students"
 end
 
-# Nothing happens until we call the methods
 students = input_students
+#nothing happens until we call the methods
 print_header
 print(students)
 print_footer(students)
-
-
-
