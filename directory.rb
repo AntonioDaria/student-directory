19811cc54009390158e1d458a55723e243456726
Antonio <antoniodaria@msn.com>
2018-08-30 15:55:16 +0100

    Implementing step 10 more refactoring

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+@students = []
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return 4 times"
@@ -15,42 +16,48 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << { name: name, country_of_birth: cob, hobby: hobby, cohort: cohort }
-    puts "Now we have #{students.count} students"
+    @students << { name: name, country_of_birth: cob, hobby: hobby, cohort: cohort }
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.strip
     cob = gets.strip
     hobby = gets.strip
     cohort = gets.strip
   end
-  # return the array of students
-  students
+end
+
+def print_menu
+  # 1. print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9 Exit"
+end
+
+def show_students
+  print_header
+  empty_list_message
+  default_cohort
+  print_by_cohort
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit 
+    else
+      puts "I don't know what you meant, try again"
+  end 
 end
 
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9 Exit"
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        empty_list_message(students)
-        default_cohort(students)
-        print_by_cohort(students)
-        print_footer(students)
-      when "9"
-        exit # this will cause the program to terminate
-      else
-        puts "I don't know what you meant, try again"
-    end 
+    print_menu
+    process(gets.chomp)
   end
 end
 
@@ -59,7 +66,7 @@ def print_header
   puts "-------------".center(50)
 end
 
-def print(students)
+def print
   count = 1
   while count <= 1 do
       for i in students do
@@ -69,35 +76,35 @@ def print(students)
   end
 end
 
-def print_by_cohort(students)
-  students.map do | student |
+def print_by_cohort
+  @students.map do | student |
     if student[:cohort] == "april"
         puts "#{student[:name]} (#{student[:cohort]}) (#{student[:country_of_birth]}) (#{student[:hobby]})".center(55)
     end
   end
 end
 
-def default_cohort(students)
+def default_cohort
   cohort = "april"
-  students.map do |student|
+  @students.map do |student|
       if student[:cohort].empty? 
           puts "#{student[:name]} (#{cohort}) (#{student[:country_of_birth]}) (#{student[:hobby]})".center(55)
       end
   end
 end
 
-def empty_list_message(students)
-  if students.empty?
+def empty_list_message
+  if @students.empty?
       puts "no values inserted".center(50)
   end
 end
 
-def print_footer(students)
+def print_footer
   puts "-------------".center(50)
-  if students.count < 2
-      puts "Overall, we have #{students.count} great student".center(50)
+  if @students.count < 2
+      puts "Overall, we have #{@students.count} great student".center(50)
   else
-      puts "Overall, we have #{students.count} great students".center(50)
+      puts "Overall, we have #{@students.count} great students".center(50)
   end
 end
 
